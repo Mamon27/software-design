e0a09486c30ef030ed58bed843dfecd98b674b56
Yarosh Volodymyr <mamon4uk27@gmail.com>
2024-04-18 18:11:09 +0300

    lab01

--- lab01/zoo_test.py
+++ lab01/zoo_test.py
@@ -7,21 +7,16 @@ class TestAnimalManager(unittest.TestCase):
         animals = []
         manager = AnimalManager(None, animals)
         
-        # Создаем заглушку для атрибута animal_window
         manager.animal_window = MagicMock()
         
-        # Создаем поддельные объекты для add_animal_entry и species_var
         manager.add_animal_entry = MagicMock()
         manager.species_var = MagicMock()
         
-        # Задаем значения, которые будут возвращать методы get() этих объектов
         manager.add_animal_entry.get.return_value = "Лев"
         manager.species_var.get.return_value = "Хижаки"
         
-        # Добавляем животное
         manager.confirm_add_animal()
         
-        # Проверяем, что список animals содержит одно животное
         self.assertEqual(len(animals), 1)
 
 if __name__ == '__main__':
